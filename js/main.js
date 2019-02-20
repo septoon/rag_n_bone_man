4cf61984cc397bab441f577c6759d891cd8ed76a
septoon <41443306+septoon@users.noreply.github.com>
2019-02-20 21:18:16 +0300

    start commit

---
+++ js/main.js
@@ -0,0 +1,69 @@
+
+// const flashingObjects = () => {
+//     for (let i=1; i < 999; i++) {
+//         $(".elips").fadeOut(2800).fadeIn(2600);
+//         $(".plus").fadeOut(2400).fadeIn(2200);
+//         $(".lines-dot").fadeOut(2400).fadeIn(2200);
+//         $(".line-dot-circle").fadeOut(2200).fadeIn(2000);
+//         $(".tringles").fadeOut(2300).fadeIn(2100);
+//         $(".plus-dots").fadeOut(2100).fadeIn(1900);
+//         $(".light-blue").fadeOut(2700).fadeIn(2500);
+//     }
+// };
+// window.setTimeout( flashingObjects, 1500 );
+
+// Slider
+// let slideIndex = 1;
+// showSlides(slideIndex);
+
+// function plusSlide(n) {
+//     showSlides(slideIndex += n);
+// };
+
+// function currentSlide(n) {
+//     showSlides(slideIndex = n);
+// };
+
+// function showSlides(n) {
+//     let slides = document.getElementsByClassName("slide");
+    
+//     if (n > slides.length) {
+//         slideIndex = 1;
+//     } 
+//     if (n < 1) {
+//         slideIndex = slides.length;
+//     }
+//     for (let i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     slides[slideIndex-1].style.display = "block";
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Automatic slideShow
+
+// var slideIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}
+//   slides[slideIndex-1].style.display = "block";
+//   setTimeout(showSlides, 2000); // Change image every 2 seconds
+// } 
